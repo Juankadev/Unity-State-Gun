36e25795bfe8a20c010ac41aaea32dde1500348a
Juan Cruz Rey <54191222+Juankadev@users.noreply.github.com>
2023-01-19 15:23:57 -0300

    Project

---
+++ Project/Assets/Scripts/DoorScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    public bool isDoorOpen = false;
+    public int nextLevelID;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (isDoorOpen)
+            {
+                FindObjectOfType<AudioManager>().Play("win");
+                GameManager.instance.NextLevel(nextLevelID);
+            }
+        }
+    }
+}
